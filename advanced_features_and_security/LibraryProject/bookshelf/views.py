1731188ada2291ae0e111f8044030fa82515d3f6
Mahider Tsegaye <Mahidernardos@gmail.com>
2025-09-13 22:56:38 +0300

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import permission_required
 from .models import Book
 # from .forms import BookForm
 from .forms import BookSearchForm
+from .forms import ExampleForm
 
 # All input is validated using Django forms to prevent SQL injection.
 # CSRF tokens are required in all POST forms.
@@ -16,24 +17,24 @@ def book_list(request):
 @permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
     if request.method == 'POST':
-        form = BookSearchForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookSearchForm()
+        form = ExampleForm()
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        form = BookSearchForm(request.POST, instance=book)
+        form = ExampleForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookSearchForm(instance=book)
+        form = ExampleForm(instance=book)
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
@@ -42,7 +43,7 @@ def book_delete(request, pk):
     book.delete()
     return redirect('book_list')
 def search_books(request):
-    form = BookSearchForm(request.GET or None)
+    form = ExampleForm(request.GET or None)
     books = Book.objects.none()
     if form.is_valid():
         query = form.cleaned_data['query']
